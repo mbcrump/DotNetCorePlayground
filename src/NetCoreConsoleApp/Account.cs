dcab461d0734c7a7031f6d7ef40bfc8aaa152681
Michael Crump <mbcrump29@gmail.com>
2017-02-07 17:22:22 -0800

    Add project files.

---
+++ src/NetCoreConsoleApp/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetCoreConsoleApp
+{
+    public class Account
+    {
+        public bool Active { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Email { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
---
+++ src/NetCoreConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace NetCoreConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Account account = new Account
+            {
+                Email = "michael@blah.com",
+                Active = true,
+                CreatedDate = new DateTime(2017, 2, 20, 0, 0, 0, DateTimeKind.Utc),
+                Roles = new List<string>
+                {
+                     "User",
+                     "Admin"
+                }
+            };
+
+            string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+            // {
+            //   "Email": "james@example.com",
+            //   "Active": true,
+            //   "CreatedDate": "2013-01-20T00:00:00Z",
+            //   "Roles": [
+            //     "User",
+            //     "Admin"
+            //   ]
+            // }
+
+            Console.WriteLine(json);
+
+        }
+    }
+}
